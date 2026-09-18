21d142ad1134b76aa2a3aab5e91394a0832fc7c3
Johannes Arnold <jarnold@b1-systems.de>
2026-09-19 01:19:14 +0200

    Change green color

--- internal/bridge/bridge.go
+++ internal/bridge/bridge.go
@@ -301,7 +301,7 @@ func (b *Bridge) handleMatrixMessage(sender, mxid, body string) {
 	mxidEscaped := jsonEscape(mxid)
 
 	tellraw := fmt.Sprintf(
-		`tellraw @a [{"text":"[Matrix] ","color":"dark_green","hoverEvent":{"action":"show_text","value":"%s"}},{"text":"<%s> ","color":"white","hoverEvent":{"action":"show_text","value":"%s"}},{"text":"%s","color":"white"}]`,
+		`tellraw @a [{"text":"[Matrix] ","color":"green","hoverEvent":{"action":"show_text","value":"%s"}},{"text":"<%s> ","color":"white","hoverEvent":{"action":"show_text","value":"%s"}},{"text":"%s","color":"white"}]`,
 		mxidEscaped,
 		senderEscaped,
 		mxidEscaped,
